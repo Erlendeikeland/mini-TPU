a499edbfe1fed6575a72982e063968f18d58ed1a
Erlendeikeland <Erlend132@gmail.com>
2024-10-28 12:16:45 +0100

    Update tb

--- vhdl/tb/accumulator_tb.vhd
+++ vhdl/tb/accumulator_tb.vhd
@@ -34,6 +34,14 @@ architecture behave of accumulator_tb is
         (0, 0, 0, 16)
     );
 
+    type result_matrix_t is array (0 to (SIZE - 1), 0 to (SIZE - 1)) of natural;
+    constant result_matrix : result_matrix_t := (
+        (1, 2, 3, 4),
+        (5, 6, 7, 8),
+        (9, 10, 11, 12),
+        (13, 14, 15, 16)
+    );
+
 begin
 
     accumulator_inst: entity work.accumulator
@@ -75,9 +83,42 @@ begin
         wait for CLK_PERIOD * 5;
         
         -- Read from accumulator
-        for i in 0 to ((WIDTH * 2) - 2) loop
+        for i in 0 to (WIDTH - 1) loop
             read_address <= i;
+            wait for CLK_PERIOD * 2;
+            for j in 0 to (WIDTH - 1) loop
+                assert read_data(j) = std_logic_vector(to_unsigned(result_matrix(i, j), MAX_ACCUM_WIDTH)) report "Expected " & integer'image(result_matrix(i, j)) & " but got " & integer'image(to_integer(unsigned(read_data(j)))) severity error;
+            end loop;
+        end loop;
+
+        wait for CLK_PERIOD * 5;
+
+        -- Write to accumulator with accumulating
+        for i in 0 to ((WIDTH * 2) - 2) loop
+            if i < WIDTH then
+                write_enable <= '1';
+                write_address <= i;
+                accumulate <= '1';
+            else
+                write_enable <= '0';
+                write_address <= 0;
+                accumulate <= '0';
+            end if;
+            for j in 0 to (WIDTH - 1) loop
+                write_data(j) <= std_logic_vector(to_unsigned(data_matrix(i, j), MAX_ACCUM_WIDTH));
+            end loop;
             wait for CLK_PERIOD;
+        end loop;
+
+        wait for CLK_PERIOD * 5;
+
+        -- Read from accumulator
+        for i in 0 to (WIDTH - 1) loop
+            read_address <= i;
+            wait for CLK_PERIOD * 2;
+            for j in 0 to (WIDTH - 1) loop
+                assert read_data(j) = std_logic_vector(to_unsigned(result_matrix(i, j) * 2, MAX_ACCUM_WIDTH)) report "Expected " & integer'image(result_matrix(i, j)) & " but got " & integer'image(to_integer(unsigned(read_data(j)))) severity error;
+            end loop;
         end loop;
 
         stop;
